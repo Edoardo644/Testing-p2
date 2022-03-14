4f1c6e01112ba8fe791d153b49b5de8cb520150c
Edoardo644 <percyjackson.e4@gmail.com>
2022-03-14 11:08:54 +0100

    gitignore modified

---
+++ Class1.cs
@@ -0,0 +1,15 @@
+﻿//Hello World! programma in C#.
+using System;
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Premi un tasto per terminare il programma");
+            
+        }
+    }
+}
